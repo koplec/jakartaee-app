7026f39bee335ab8ee84fcd7323b3226f6e1c71e
ftg <ftg@example.prv>
2023-11-28 19:30:01 +0900

    コメント追加

--- src/main/java/prv/koplec/jaxrs/MyResourceConfig.java
+++ src/main/java/prv/koplec/jaxrs/MyResourceConfig.java
@@ -5,6 +5,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class MyResourceConfig extends ResourceConfig{
     public MyResourceConfig(){
         packages("prv.koplec.jaxrs");
+
+        //@Providerがないときは、下記でExceptionMapperを登録する？
         register(WebApplicationExceptionMapper.class);
     }
     
--- src/main/java/prv/koplec/jaxrs/WebApplicationExceptionMapper.java
+++ src/main/java/prv/koplec/jaxrs/WebApplicationExceptionMapper.java
@@ -9,7 +9,6 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
-@Provider
 public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
     @Override
     public Response toResponse(WebApplicationException exception) {
