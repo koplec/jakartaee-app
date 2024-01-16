9c8d9ac1b6c275990eaed63d06b441a3be8c5d74
koplec <koplec@github.com>
2024-01-16 22:42:33 +0900

    lifecycleを追加

--- src/main/java/prv/koplec/jaxrs_todo/TodoAppBinder.java
+++ src/main/java/prv/koplec/jaxrs_todo/TodoAppBinder.java
@@ -2,6 +2,7 @@ package prv.koplec.jaxrs_todo;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
+import jakarta.inject.Singleton;
 import prv.koplec.jaxrs_todo.services.AuthService;
 import prv.koplec.jaxrs_todo.services.TodoService;
 import prv.koplec.jaxrs_todo.services.TodoServiceImpl;
@@ -12,8 +13,12 @@ public class TodoAppBinder extends AbstractBinder{
     @Override
     protected void configure() {
         
-        bindAsContract(AuthService.class);
-        bind(TodoServiceImpl.class).to(TodoService.class);
-        bind(UserServiceImpl.class).to(UserService.class);
+        bindAsContract(AuthService.class).in(Singleton.class);
+        bind(TodoServiceImpl.class).to(TodoService.class)
+            .in(Singleton.class);
+        bind(UserServiceImpl.class).to(UserService.class)
+            .in(Singleton.class);
+        //ApplicationScopedなら、in(Singleton.class)を指定する
+        //RequestScoped, SessionScopedに対応するものはなさそう
     }
 }
